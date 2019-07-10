ae2bb0f9926b1fe446bf86e68c66fdc53acf68d5
anyongliang <anyongliang@icreations.com.cn>
2019-07-10 10:18:18 +0800

    新建：上传已经整理好的代码

---
+++ src/main/java/com/ethendev/wopihost/WopihostApplication.java
@@ -0,0 +1,16 @@
+package com.ethendev.wopihost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ethendev on 2017/4/15.
+ * updated by Rock-Ayl on 2019-7-10
+ */
+@SpringBootApplication
+public class WopihostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WopihostApplication.class, args);
+	}
+}
